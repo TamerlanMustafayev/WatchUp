db6551a8fd4e7aa43731bc9d7f0f65d5ded61839
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2023-06-20 20:25:42 -0400

    Fix bugs step1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import Products from "./components/Products"
@@ -6,17 +8,22 @@ import AboutUs from './components/AboutUs';
 import Testimonials from './components/Testimonials';
 import Featured from './components/Featured';
 import Contact from './components/Contact';
+
+
+
 function App() {
   return (
+
     <div className="App">
-      <Header />
-      <Hero />
-      <Featured />
-      <Products />
-      <Testimonials />
-      <AboutUs />
-      <Contact />
-    </div>
+        <Header />
+        <Hero />
+        <Featured />
+        <Products />
+        <Testimonials />
+        <AboutUs />
+        <Contact />
+      </div>
+    
   );
 }
 
---
+++ src/components/products/Maximilian.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Maximilian() {
+    return (
+
+        <>
+            <h2>Hello</h2>
+        </>
+    )
+}
+
+
+export default Maximilian
